6e7fca5e3836ad61f80292f40cb29c04ce9dc305
luifer991 <luifer991@gmail.com>
2025-07-17 18:06:44 -0500

    add experiences and project collection to content.config.ts file

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,4 +12,47 @@ const contents = defineCollection({
 	}),
 });
 
-export const collections = { contents };
+enum workType {
+	Remote = 'Remote',
+	Onsite = 'Onsite',
+	Hybrid = 'Hybrid',
+}
+
+const experiences = defineCollection({
+	loader: glob({
+		pattern: '**/*.md',
+		base: 'src/content/experiences',
+	}),
+	schema: z.object({
+		company: z.string(),
+		work: z
+			.string()
+			.refine(
+				(val) => Object.values(workType).includes(val as workType),
+				{
+					message:
+						'Work type must be one of Remote, Onsite, or Hybrid.',
+				}
+			),
+		initial: z.string(),
+		final: z.string(),
+		role: z.string(),
+	}),
+});
+
+const projects = defineCollection({
+	loader: glob({
+		pattern: '**/*.md',
+		base: 'src/content/projects',
+	}),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		frontend: z.string().optional(),
+		backend: z.string().optional(),
+		link: z.string().url().optional(),
+		github: z.string().url(),
+	}),
+});
+
+export const collections = { contents, experiences, projects };
